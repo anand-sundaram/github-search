a6127c046fb58316675df9f9e3aa5bbdc01349be
Anand Sundaram <ansundaram@paypal.com>
2016-11-21 22:51:06 +0800

    removed debug log to console

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -6,10 +6,8 @@ export default Ember.Controller.extend({
 			return new Promise(function(resolve, reject){
 
 				Ember.$.getJSON('https://api.github.com/search/repositories?q=' + query).then(function(response) {
-					console.log(response);
 					resolve(response.items);
 				}, function(reason) {
-					console.log(reason);
 					reject([]);
 				});
 			});
